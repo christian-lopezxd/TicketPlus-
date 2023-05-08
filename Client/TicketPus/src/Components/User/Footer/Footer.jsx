73da6c352903cac4aa1e895f68b2f73ff1a94938
christian-lopezxd <00179320@uca.edu.sv>
2023-05-08 05:02:53 -0600

    feat: Add React architecture :pencil2:

---
+++ Client/TicketPus/src/Components/User/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-grissoft text-white h-16">
+      <h1>Me ardi</h1>
+    </footer>
+  );
+};
+
+export default Footer;
