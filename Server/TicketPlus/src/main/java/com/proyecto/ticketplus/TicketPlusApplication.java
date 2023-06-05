e51bad90124eb67f7c9331298ad1adf3de683f67
Ernesto-Canales <00051120@uca.edu.sv>
2023-06-05 00:10:03 -0600

    Add: Creating base project :tada:
    Creating basic Spring Boot project with needed dependencies

---
+++ Server/TicketPlus/src/main/java/com/proyecto/ticketplus/TicketPlusApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.ticketplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketPlusApplication.class, args);
+	}
+
+}
