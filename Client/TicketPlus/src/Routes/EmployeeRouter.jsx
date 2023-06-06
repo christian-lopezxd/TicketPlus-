c569059ad692cb195d71cbe3437e51641cc82036
OscarCornejo00 <00223019@uca.edu.sv>
2023-06-05 22:19:59 -0600

    Add: LobbyEmployee view

--- Client/TicketPlus/src/Routes/EmployeeRouter.jsx
+++ Client/TicketPlus/src/Routes/EmployeeRouter.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Routes } from "react-router";
 import { Route } from "react-router";
-import Lobby from './../Pages/User/Lobby'
+import LobbyEmployee from './../Pages/User/LobbyEmployee'
 import EventInfo from './../Pages/User/EventInfo'
 
 const EmployeeRouter = () => {
     return (
         <Routes>
-            <Route path="/" element={<Lobby/>}></Route>
+            <Route path="/" element={<LobbyEmployee/>}></Route>
             <Route path="/Event" element={<EventInfo/>}></Route>
 
         </Routes>
