9298cec86a59cc7e3dbcd0a663c2d904101ec1f9
Ernesto-Canales <00051120@uca.edu.sv>
2023-06-06 22:37:55 -0600

    Add: Creating basic WebSecurityConfiguration.java :lock:
    Creating basic script for Web Security, just PasswordEncoder ATM.

--- Server/TicketPlus/src/main/java/com/proyecto/ticketplus/TicketPlusApplication.java
+++ Server/TicketPlus/src/main/java/com/proyecto/ticketplus/TicketPlusApplication.java
@@ -1,13 +1,21 @@
 package com.proyecto.ticketplus;
 
+import java.security.SecureRandom;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class TicketPlusApplication {
-
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(5, new SecureRandom());
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TicketPlusApplication.class, args);
 	}
-
 }
