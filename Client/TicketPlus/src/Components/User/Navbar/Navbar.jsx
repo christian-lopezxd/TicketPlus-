9a9419bd39d0b281f39c89dc4652f9ac04f9eb52
OscarCornejo00 <00223019@uca.edu.sv>
2023-06-08 01:29:17 -0600

    Add: Menu Mobile

--- Client/TicketPlus/src/Components/User/Navbar/Navbar.jsx
+++ Client/TicketPlus/src/Components/User/Navbar/Navbar.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import Anchors from "./Anchors/Anchors";
-
-
+import AnchorsMobile from "./Anchors/AnchorsMobile";
 
 const Navbar = () => {
   return (
-    <div className="bg-grissoft h-20 flex justify-between p-2 items-center sticky top-0 z-50">
-      <NavLink to="/"> <p className="text-white font-lilita text-5xl">Ticketplus</p> </NavLink >
-      <Anchors />
-    </div>
+    <nav className="bg-grissoft p-5 flex items-center justify-between">
+      <div>
+        <NavLink to="/"> <p className="text-white font-lilita text-5xl">Ticketplus</p> </NavLink >
+      </div>
+      <div>
+        <AnchorsMobile />
+      </div>
+      <div>
+        <Anchors />
+      </div>
+    </nav>
   );
 };
 
