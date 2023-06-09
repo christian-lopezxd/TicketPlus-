dd8254c9309ac845895c66a045eb3b03b3aa4795
OscarCornejo00 <00223019@uca.edu.sv>
2023-06-08 22:26:21 -0600

    Add: EventInfo for Analyst View

--- Client/TicketPlus/src/Routes/AnalystRouter.jsx
+++ Client/TicketPlus/src/Routes/AnalystRouter.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Routes } from "react-router";
 import { Route } from "react-router";
 import Lobby from './../Pages/Analyst/Lobby'
-import EventInfo from './../Pages/User/EventInfo'
+import EventInfo from './../Pages/Analyst/EventInfo'
 
 const AnalystRouter = () => {
     return (
