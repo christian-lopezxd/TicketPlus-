774989d68db93070f066ae2e4647204437cb1835
christian-lopezxd <00179320@uca.edu.sv>
2023-06-09 23:04:23 -0600

    Add: Responsive Share Ticket :pencil2:

--- Client/TicketPlus/src/Pages/User/RedeemTicket.jsx
+++ Client/TicketPlus/src/Pages/User/RedeemTicket.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Footer from "../../Components/User/Footer/Footer";
+import Navbar from "../../Components/User/Navbar/Navbar";
+import QRRedeem from "../../Components/User/QRRedeem/QRRedeem";
 
 const RedeemTicket = () =>{
     return(
-        <section>
-            
+        <section className="bg-backgroundicons bg-repeat ">
+            <Navbar/>
+            <QRRedeem title="Scan this QR to enter the event"/>
+            <Footer />
         </section>
     )
 }
