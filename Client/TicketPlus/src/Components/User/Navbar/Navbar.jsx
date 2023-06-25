7b836811841b911014517ea7b9932476d8a337ec
christian-lopezxd <00179320@uca.edu.sv>
2023-06-25 01:55:37 -0600

    Fix: Fetch data from dummy :beers:

--- Client/TicketPlus/src/Components/User/Navbar/Navbar.jsx
+++ Client/TicketPlus/src/Components/User/Navbar/Navbar.jsx
@@ -2,17 +2,13 @@ import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import Anchors from "./Anchors/Anchors";
 import AnchorsMobile from "./Anchors/AnchorsMobile";
-import { ImCross, ImMenu } from "react-icons/im";
-import { FiUser } from 'react-icons/fi';
-import { FiCalendar } from 'react-icons/fi';
-import { FiAlertCircle } from 'react-icons/fi';
-import { IoTicketOutline } from 'react-icons/io5';
+
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);  
   return (
 
-    <div className="bg-grissoft h-20 flex justify-between px-2 items-center sticky top-0 z-50">
+    <div className="bg-grissoft h-20 flex justify-between px-2 items-center sticky top-[0] z-[99]">
       <NavLink to="/"> <p className="text-white font-lilita text-5xl">Ticketplus</p> </NavLink >
 
       <Anchors />
