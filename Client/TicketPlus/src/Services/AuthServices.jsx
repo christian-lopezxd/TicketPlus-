c48f540f34fe2b41451b3b73db62f233e00974bf
christian-lopezxd <00179320@uca.edu.sv>
2023-07-02 06:28:41 -0600

    Add: sign in, sign up and verify account added :sparkles:

--- Client/TicketPlus/src/Services/AuthServices.jsx
+++ Client/TicketPlus/src/Services/AuthServices.jsx
@@ -41,7 +41,7 @@ AuthServices.getUserInfo = async (token) => {
 
 }
 
-AuthServices.Register = async(rname, email, password) => {
+AuthServices.Register = async(name, email, password, navigate) => {
     
   try{
       const response = await axios.post(`${url}/auth/sign-up/password`, {
@@ -50,15 +50,47 @@ AuthServices.Register = async(rname, email, password) => {
           password,
 
         }) 
-
+        console.log(response)
         
-        alert(response.data); 
+        alert(response.data.message);
+        if(response.status== 200){
+          navigate("/success")
+        } 
+        if(response.status == 409){
+          alert("Revisa bien los campos")
+        } 
+
        
-      return response.data
+      return response.data.message
       
   }catch(error){
       throw error
   }
+
+  
+
+}
+
+AuthServices.Verify = async (UUID) => {
+
+  try{
+    const response = await axios.patch(`${url}/guest/user/toggle-verify/${UUID}`, {
+
+      }) 
+      console.log(response)
+      
+      alert(response.data.message);
+      if(response.status== 200){
+        navigate("/")
+      } 
+      
+
+     
+    return response.data
+    
+}catch(error){
+    throw error
+}
 
 }
 
